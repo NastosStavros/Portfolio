b53e64891010bd9e5bf4969df8a3f291591f0dae
NastosStavros <n.stavros@outlook.de>
2024-09-25 18:02:44 +0200

    php_error_catch

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -6,32 +6,41 @@ switch ($_SERVER['REQUEST_METHOD']) {
         header("Access-Control-Allow-Methods: POST");
         header("Access-Control-Allow-Headers: content-type");
         exit;
-        case("POST"): //Send the email;
-            header("Access-Control-Allow-Origin: *");
-            // Payload is not send to $_POST Variable,
-            // is send to php:input as a text
-            $json = file_get_contents('php://input');
-            //parse the Payload from text format to Object
-            $params = json_decode($json);
-    
-            $email = $params->email;
-            $name = $params->name;
-            $message = $params->message;
-    
-            $recipient = 's_nastos@icloud.com';  
-            $subject = "Contact From <$email>";
-            $message = "From:" . $name . "<br>" . $message ;
-    
-            $headers   = array();
-            $headers[] = 'MIME-Version: 1.0';
-            $headers[] = 'Content-type: text/html; charset=utf-8';
+    case("POST"): //Send the email;
+        header("Access-Control-Allow-Origin: *");
 
-            // Additional headers
-            $headers[] = "From: noreply@mywebsite.com";
+        // Get the JSON input
+        $json = file_get_contents('php://input');
+        $params = json_decode($json);
 
-            mail($recipient, $subject, $message, implode("\r\n", $headers));
-            break;
-        default: //Reject any non POST or OPTIONS requests.
-            header("Allow: POST", true, 405);
-            exit;
-    } 
+        // Extract the form data
+        $email = $params->email;
+        $name = $params->name;
+        $message = $params->message;
+
+        // Prepare the email
+        $recipient = 's_nastos@icloud.com';  
+        $subject = "Contact From <$email>";
+        $message = "From: " . $name . "<br>" . $message ;
+
+        $headers = array();
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf-8';
+        $headers[] = "From: info@stavros-nastos.com"; // Verwende deine Domain
+
+        // Try to send the email
+        $success = mail($recipient, $subject, $message, implode("\r\n", $headers));
+
+        // Check if mail was sent
+        if ($success) {
+            error_log("E-Mail wurde erfolgreich gesendet.");
+            echo json_encode(['status' => 'success', 'message' => 'E-Mail wurde erfolgreich gesendet.']);
+        } else {
+            error_log("Fehler beim Senden der E-Mail.");
+            echo json_encode(['status' => 'error', 'message' => 'Fehler beim Senden der E-Mail.']);
+        }
+        break;
+    default: //Reject any non POST or OPTIONS requests.
+        header("Allow: POST", true, 405);
+        exit;
+}
