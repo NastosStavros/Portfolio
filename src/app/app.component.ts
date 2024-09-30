503b7ef743fe3dffa4ecd82099b4aa474c73e8d1
NastosStavros <n.stavros@outlook.de>
2024-09-30 21:03:08 +0200

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Hier sollte 'styleUrls' anstelle von 'styleUrl' sein
 })
-export class AppComponent {
+export class AppComponent implements OnInit { // Implementiere OnInit
   title = 'my-portfolio';
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000, // Dauer der Animation in Millisekunden
+      once: true, // Animation nur einmal ausführen
+    });
+  }
 }
